f6ec192ac1d335a993311aa7a54c1eb41c036544
puddingii <67890897+puddingii@users.noreply.github.com>
2021-05-31 02:40:26 +0900

    chatting function & notification
    add the chatting function and notification
    
    notification : notification is triggered when someone enters in the chatting room

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,16 +1,62 @@
 import gulp from "gulp";
 import sass from "gulp-sass";
+import autoprefixer from "gulp-autoprefixer";
+import minifyCSS from "gulp-csso";
+import del from "del";
+import bro from "gulp-bro";
+import babel from "babelify";
 
 const paths = {
     styles: {
         src: "assets/scss/styles.scss",
         dest: "src/static/styles",
+        watch: "assets/scss/**/*.scss",
     },
+    js: {
+        src: "assets/js/main.js",
+        dest: "src/static/js",
+        watch: "assets/js/**/*.js",
+    }
 };
 
-export function styles() {
-    return gulp
+const clean = () => del(["src/static"]);
+
+const styles = () =>
+    gulp
     .src(paths.styles.src)
     .pipe(sass())
+    .pipe(
+        autoprefixer({
+        cascade: false,
+    })
+    )
+    .pipe(minifyCSS())
     .pipe(gulp.dest(paths.styles.dest));
+
+
+const js = () =>
+    gulp
+    .src(paths.js.src)
+    .pipe(
+        bro({
+            transform: [
+                babel.configure({
+                    presets: ["@babel/preset-env"],
+                }),
+                ["uglifyify", {global: true}]
+            ]
+        })
+    )
+    .pipe(gulp.dest(paths.js.dest));
+
+//변화 감지할 파일들
+const watchFiles = () => {
+    gulp.watch(paths.styles.watch, styles);
+    gulp.watch(paths.js.watch, js);
 }
+
+const dev = gulp.series(clean, styles, js, watchFiles);
+
+export const build = gulp.series(clean, styles, js);
+
+export default dev;
--- src/init.js
+++ src/init.js
@@ -1,6 +1,8 @@
 import express from "express";
 import socketIO from "socket.io";
 import morgan from "morgan";
+import events from "./events";
+import socketController from "./socketController";
 
 const PORT = 4050;
 const app = express();
@@ -8,7 +10,9 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(morgan("dev"));
 app.use(express.static(process.cwd() + "/src/static"));
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (req, res) => res.render("home", { 
+    events: JSON.stringify(events)
+}));
 
 const handleListening = () =>
     console.log(`Server running : http://localhost:${PORT}`);
@@ -17,14 +21,5 @@ const server = app.listen(PORT, handleListening);
 
 const io = socketIO(server);
 
-io.on("connection", (socket) => {  //client가 접속하면 connection이라는 event를 발생시킴. 그리고 socket은 하나의 socket임
-    socket.on("newMessage", ({ message }) => {   //newMessage가 들어오면 다른 곳에 messageNotifi이벤트를 broadcast함.
-        socket.broadcast.emit("messageNotif", { 
-            message, 
-            nickname:socket.nickname || "Anon"
-        });
-    });
-    socket.on("setNickname", ({nickname}) => {
-        socket.nickname = nickname;
-    });
-});
+io.on("connection", (socket) => socketController(socket));
+//client가 접속하면 connection이라는 event를 발생시킴. 그리고 socket은 하나의 socket임
